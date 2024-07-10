6434b9a70d5cf7ed246a4bb72dabdf197783e4d7
Gustavo Oliveira <guvo@Laptop-de-Gustavo.local>
2024-07-10 19:59:15 -0300

    add: first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*",
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "import/no-unresolved": 0,
+    "indent": ["error", 2],
+    "require-jsdoc": 0,
+    "object-curly-spacing": ["error", "always"],
+    "max-len": ["error", {
+      "code": 120,
+      "ignoreComments": true,
+      "ignoreStrings": true,
+    }],
+    "@typescript-eslint/no-unused-vars": ["error", { "ignoreRestSiblings": true }],
+  },
+};
